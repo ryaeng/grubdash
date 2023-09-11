f508c78e894daa0c8cc415b8cad6bfcf7965a95b
Ryan C. England <rcengland@gmail.com>
2023-09-11 14:51:34 -0400

    Added create to /:orderId and included input validation.

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -7,10 +7,91 @@ const orders = require(path.resolve("src/data/orders-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /orders handlers needed to make the tests pass
+const bodyDataHas = (propertyName) => {
+    return (req, res, next) => {
+        const { data = {} } = req.body;
+        if (data[propertyName] && data[propertyName] !== "") {
+            return next();
+        }
+        next({
+            status: 400,
+            message: `Order must include a ${propertyName}`
+        })
+    }
+}
+
+const dishesPropertyIsValid = (req, res, next) => {
+    const { data: { dishes } = {} } = req.body;
+    if (Array.isArray(dishes) && dishes.length > 0) {
+        return next();
+    }
+    next({
+        status: 400,
+        message: `Order must include one dish`,
+    });
+}
+
+const dishQuantityPropertyIsValidNumber = (req, res, next) => {
+    const { data: { dishes } = {} } = req.body;
+    for (let index = 0; index < dishes.length; index++) {
+        const quantity = dishes[index].quantity;
+        if (quantity && quantity > 0 && Number.isInteger(quantity)) {
+            next()
+        }
+        return next({
+            status: 400,
+            message: `Dish ${index} must have a quantity that is an integer greater than 0`,
+        });
+    }
+}
+
+const orderExists = (req, res, next) => {
+    const { orderId } = req.params;
+    const foundOrder = orders.find((order) => order.id === orderId);
+    if (foundOrder) {
+        res.locals.order = foundOrder;
+        return next()
+    }
+    next({
+        status: 404,
+        message: `Order does not exist: ${orderId}`,
+    });
+}
+
+const create = (req, res) => {
+    const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
+    const newOrder = {
+        id: nextId,
+        deliverTo,
+        mobileNumber,
+        status,
+        dishes,
+    };
+    orders.push(newOrder);
+    res.status(201).json({ data: newOrder });
+}
+
 const list = (req, res) => {
     res.json({ data: orders });
 }
 
+const read = (req, res) => {
+    res.json({ data: res.locals.order });
+}
+
 module.exports = {
-    list
+    create: [
+        bodyDataHas("deliverTo"),
+        bodyDataHas("mobileNumber"),
+        bodyDataHas("status"),
+        bodyDataHas("dishes"),
+        dishesPropertyIsValid,
+        dishQuantityPropertyIsValidNumber,
+        create
+    ],
+    list,
+    read: [
+        orderExists,
+        read
+    ]
 }
--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -4,8 +4,14 @@ const methodNotAllowed = require("../errors/methodNotAllowed");
 
 // TODO: Implement the /orders routes needed to make the tests pass
 router
+    .route("/:orderId")
+    .get(controller.read)
+    .all(methodNotAllowed);
+
+router
     .route("/")
     .get(controller.list)
+    .post(controller.create)
     .all(methodNotAllowed);
 
 module.exports = router;
