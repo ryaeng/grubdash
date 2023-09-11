1e0796416a126c7c47b9cb3198e669bfdbcd56fe
Ryan C. England <rcengland@gmail.com>
2023-09-11 09:57:32 -0400

    Added create to /dishes & input validation.

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,10 +7,55 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+const bodyDataHas = (propertyName) => {
+    return (req, res, next) => {
+        const { data = {} } = req.body;
+        if (data[propertyName] && data[propertyName] !== "") {
+            return next();
+        }
+        next({
+            status: 400,
+            message: `Dish must include a ${propertyName}`
+        })
+    }
+}
+
+const priceIsValidNumber = (req, res, next) => {
+    const { data: { price } = {} } = req.body;
+    if (price <= 0 || !Number.isInteger(price)) {
+        return next({
+            status: 400,
+            message: "Dish must have a price that is an integer greater than 0",
+        });
+    }
+    next();
+}
+
+const create = (req, res) => {
+    const { data: { name, description, price, image_url } = {} } = req.body;
+    const newDish = {
+        id: nextId(),
+        name,
+        description,
+        price,
+        image_url,
+    };
+    dishes.push(newDish);
+    res.status(201).json({ data: newDish });
+}
+
 const list = (req, res) => {
     res.json({ data: { dishes } });
 }
 
 module.exports = {
+    create: [
+        bodyDataHas("name"),
+        bodyDataHas("description"),
+        bodyDataHas("price"),
+        bodyDataHas("image_url"),
+        priceIsValidNumber,
+        create
+    ],
     list,
 }
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -4,6 +4,7 @@ const controller = require("./dishes.controller");
 // TODO: Implement the /dishes routes needed to make the tests pass
 router
     .route("/")
-    .get(controller.list);
+    .get(controller.list)
+    .post(controller.create);
 
 module.exports = router;
